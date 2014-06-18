1c5a9bccb1712328a590231970b4fb39c78d432b
Joel Trost <joelt@zillow.com>
2014-06-17 21:40:47 -0700

    testing hookshot

--- hook.js
+++ hook.js
@@ -1,5 +1,6 @@
 // Listen on port 9001
-var gith = require('gith').create( 9003 );
+//var gith = require('gith').create( 9003 );
+var hookshot = require('hookshot');
 // Import execFile, to run our bash script
 
 var execOptions = {
@@ -8,19 +9,22 @@ var execOptions = {
 
 var execFile = require('child_process').execFile;
 
-gith({
-    repo: 'thecaddy/qarth',
-    branch: 'master'
-}).on( 'all', function( payload ) {
-    console.log('Executing Payload:', payload);
-    if( payload.branch === 'master' )
-    {
-            // Exec a shell script
-            execFile('deploy.sh', execOptions, function(error, stdout, stderr) {
-                    // Log success in some manner
-                    console.log( 'EXEC COMPLETE' );
-            });
-    }
-});
+// gith({
+//     repo: 'thecaddy/qarth'
+// }).on( 'all', function( payload ) {
+//     console.log('Executing Payload:', payload);
+//     if( payload.branch === 'master' )
+//     {
+//             // Exec a shell script
+//             execFile('deploy.sh', execOptions, function(error, stdout, stderr) {
+//                     // Log success in some manner
+//                     console.log( 'EXEC COMPLETE' );
+//             });
+//     }
+// });
+
+hookshot('refs/heads/master', function(info) {
+  console.log('made it', info);
+}).listen(9003);
 
 console.log('Hook Started');
