280418bef25af9c919a9c74589df28395cb4d433
Joel Trost <joelt@zillow.com>
2014-06-17 20:08:44 -0700

    added auto deploy scripts

---
+++ autodeploy.sh
@@ -0,0 +1,33 @@
+################################################################################
+################################################################################
+################################################################################
+#sh autodeploy.sh
+#!/usr/bin/env bash
+
+#install git
+sudo apt-get install git
+
+#install docker
+sudo apt-get update
+sudo apt-get install docker.io
+sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
+
+#install node
+sudo apt-get install -y nodejs npm
+
+#get app
+mkdir /home/app
+cd /home/app
+sudo git clone https://github.com/thecaddy/qarth-dock
+cd qarth-dock
+#install gith
+sudo npm install
+
+#turn it back on if it crashes for any reason
+sudo npm install -g forever
+forever start hook.js
+
+################################################################################
+################################################################################
+################################################################################
---
+++ hook.js
@@ -0,0 +1,23 @@
+// Listen on port 9001
+var gith = require('gith').create( 9003 );
+// Import execFile, to run our bash script
+
+var execOptions = {
+     //maxBuffer: 1024 * 1024 // 1mb
+}
+
+var execFile = require('child_process').execFile;
+
+gith({
+    repo: 'thecaddy/qarth',
+    branch: 'master'
+}).on( 'all', function( payload ) {
+    if( payload.branch === 'master' )
+    {
+            // Exec a shell script
+            execFile('deploy.sh', execOptions, function(error, stdout, stderr) {
+                    // Log success in some manner
+                    console.log( 'EXEC COMPLETE' );
+            });
+    }
+});
